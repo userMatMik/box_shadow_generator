3975ba26d190281b55647aa7e08a126f0ddadf59
userMatMik <106982809+userMatMik@users.noreply.github.com>
2022-08-27 21:53:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,38 @@ const box = document.querySelector('#box');
 //! box-shadow: none|h-offset v-offset blur spread color |inset|initial|inherit;
 //! document.getElementById("shadow").style["boxShadow"] = "0 0 5px #999999";
 
+//? Get the 2 left digits of the hex color code and convert to decimal value to get the red color level.
+//? Get the 2 middle digits of the hex color code and convert to decimal value to get the green color level.
+//? Get the 2 right digits of the hex color code and convert to decimal value to get the blue color level
 
 
-function updateShadow(hOffset, vOffset, blur) {
-    console.log(vOffset)
-    console.log(hOffset)
-    console.log(blur)
+function hexToRGB(hex) {
+    
+    const r = parseInt(hex.replace('#', '').slice(0, 2), 16)
+    const g = parseInt(hex.replace('#', '').slice(2, 4), 16)
+    const b = parseInt(hex.replace('#', '').slice(4, 6), 16)
+    
+    
+    return `${r}, ${g}, ${b}`
 
-    box.style['boxShadow'] = `${hOffset}px 0px 5px #999999`
+}
+
+console.log(hexToRGB('#121111'));
+
+
+function updateShadow() {
+    const hOffset = document.querySelector('#horizontal').value;
+    const vOffset = document.querySelector('#vertical').value;
+    const blur = document.querySelector('#blur').value;
+    const spread = document.querySelector('#spread').value;
+    const opacity = document.querySelector('#opacity').value;
+    const color = document.querySelector('#shadow-color').value;
+
+    console.log(color);
+
+    console.log(`${hOffset}px ${vOffset}px ${blur}px ${spread}px rgb${hexToRGB(color)}`)
+    
+    box.style['boxShadow'] = `${hOffset}px ${vOffset}px ${blur}px ${spread}px rgb(${hexToRGB(color)}, ${opacity})`
 }
 
 
@@ -22,10 +46,8 @@ horizontalInputs.forEach((input, i) => {
         // console.log(input.getAttribute('type'))
         // console.log(horizontalInputs[i])
         horizontalInputs[1-i].value = horizontalInputs[i].value;
-
-        const hOffset = input.value;
         
-        updateShadow(hOffset);
+        updateShadow();
     })
 })
 
@@ -35,9 +57,8 @@ const verticalInputs = document.querySelectorAll('input[name=vertical]');
 verticalInputs.forEach((input, i) => {
     input.addEventListener('input', () => {
         verticalInputs[1-i].value = verticalInputs[i].value; 
-        const vOffset = input.value;
-        console.log(vOffset)
-        updateShadow(vOffset);
+        
+        updateShadow();
     })
 })
 
@@ -57,8 +78,7 @@ const spreadInputs = document.querySelectorAll('input[name=spread]');
 spreadInputs.forEach((input, i) => {
     input.addEventListener('input', () => {
         spreadInputs[1-i].value = spreadInputs[i].value;
-        console.log(input.value)
-        updateShadow(input.value);
+        updateShadow();
     })
 })
 
@@ -68,10 +88,10 @@ console.log(opacityInputs)
 opacityInputs.forEach((input, i) => {
     input.addEventListener('input', () => {
         opacityInputs[1-i].value = opacityInputs[i].value;
-        updateShadow(input.value);
+        updateShadow();
     })
     
 })
 
-console.log(horizontalInputs)
+document.querySelector('#shadow-color').addEventListener('change', updateShadow )
 
